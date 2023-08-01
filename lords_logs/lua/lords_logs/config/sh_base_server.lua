c7e8fa5a942071f0cd30fe29f4522c208ea6a417
Lord Sugar <62381889+lord-sugarv2@users.noreply.github.com>
2023-08-01 10:57:37 +0100

    Update sh_base_server.lua

--- lords_logs/lua/lords_logs/config/sh_base_server.lua
+++ lords_logs/lua/lords_logs/config/sh_base_server.lua
@@ -1,4 +1,4 @@
-LLogs.SetServerConfig("ServerName", "Server Name", "Just put what your server is called.", "Lexus Networks", "STRING")
+LLogs.SetServerConfig("ServerName", "Server Name", "Just put what your server is called.", "Lord Sugar", "STRING")
 LLogs.SetServerConfig("MenuTitle", "Menu Title", "The title of the logs addon.", "%s' Logger", "STRING")
 LLogs.SetServerConfig("ConsolePrint", "Server Console Prints", "Should we print all logs to the server console?", true, "BOOL")
 LLogs.SetServerConfig("UTCTime", "Use UTC Time", "True: UTC | False: Local Time", true, "BOOL")
